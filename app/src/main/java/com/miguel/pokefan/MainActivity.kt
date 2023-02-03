0062676ef054ff560448055175bd333ec08ff7aa
Miguel Jerónimo <33616302+MiguelJeronimo@users.noreply.github.com>
2023-02-03 17:49:19 -0600

    Implementación del modo claro y obscuro con shared preferences

--- app/src/main/java/com/miguel/pokefan/MainActivity.kt
+++ app/src/main/java/com/miguel/pokefan/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.miguel.pokefan
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.miguel.pokefan.databinding.ActivityMainBinding
+import com.miguel.pokefan.utilidades.ThemesLightDark
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
+    private var temas = ThemesLightDark()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,6 +33,50 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+        // en este when validaremos si no tiene tema el shared y de no ser asi almacenar el tema claro
+        val temaa = temas.MostrarTema(AppCompatDelegate.getDefaultNightMode(), this) != null
+        if (temaa){
+            AppCompatDelegate.setDefaultNightMode(
+                temas.MostrarTema(
+                    AppCompatDelegate.MODE_NIGHT_NO,
+                    applicationContext
+                )!!.toInt()
+            )
+        } else{
+            temas.GuardarTema(AppCompatDelegate.MODE_NIGHT_NO, applicationContext)
+            AppCompatDelegate.setDefaultNightMode(
+                temas.MostrarTema(
+                    AppCompatDelegate.MODE_NIGHT_NO,
+                    applicationContext
+                )!!.toInt()
+            )
+        }
+
+        //agregando los iconos de modo claro y obscuro al switch
+        val lighTheme = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO
+        if(lighTheme){
+            binding.imgMode.setImageResource(R.drawable.icon_dark_mode)
+            binding.statusMode.isChecked = true
+        } else{
+            binding.imgMode.setImageResource(R.drawable.icon_light_mode)
+        }
+
+        binding.statusMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+                    binding.imgMode.setImageResource(R.drawable.icon_dark_mode)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    temas.GuardarTema(AppCompatDelegate.MODE_NIGHT_NO,this)
+                    print(AppCompatDelegate.getDefaultNightMode())
+                }
+            } else{
+                if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO){
+                    print(AppCompatDelegate.getDefaultNightMode())
+                    binding.imgMode.setImageResource(R.drawable.icon_light_mode)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    temas.GuardarTema(AppCompatDelegate.MODE_NIGHT_YES,this)}
+            }
         }
     }
 
