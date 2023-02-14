4909d9f5a27c694e006d0aaf720e02a5ad321935
Miguel Jerónimo <33616302+MiguelJeronimo@users.noreply.github.com>
2023-02-14 09:23:22 -0600

    Implementacion correcta del llenado de los pokemons y la navegacion entre fragmens

--- app/src/main/java/com/miguel/pokefan/MainActivity.kt
+++ app/src/main/java/com/miguel/pokefan/MainActivity.kt
@@ -25,7 +25,6 @@ import kotlinx.coroutines.*
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
---
+++ app/src/main/java/com/miguel/pokefan/viewmodels/ViewModelRecyclerView.kt
@@ -0,0 +1,8 @@
+package com.miguel.pokefan.viewmodels
+
+import android.os.Parcelable
+import androidx.lifecycle.ViewModel
+
+class ViewModelRecyclerView : ViewModel(){
+    var layoutManagerState: Parcelable? = null
+}
