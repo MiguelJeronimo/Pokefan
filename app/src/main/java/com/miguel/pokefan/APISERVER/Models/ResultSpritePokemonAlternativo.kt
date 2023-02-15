92da15c1cd72d4f0293443b5ddcb8f55b85ac656
Miguel Jerónimo <33616302+MiguelJeronimo@users.noreply.github.com>
2023-02-14 18:36:38 -0600

    Implementación del pokedex

--- app/src/main/java/com/miguel/pokefan/APISERVER/Models/ResultSpritePokemonAlternativo.kt
+++ app/src/main/java/com/miguel/pokefan/APISERVER/Models/ResultSpritePokemonAlternativo.kt
@@ -3,9 +3,12 @@ package com.miguel.pokefan.APISERVER.Models
 import com.google.gson.annotations.SerializedName
 
 //Data class para extraer la imagen alternativa del pokemon
-data class PokemonImgSprite(val sprites: Sprites, val name:String, val id: Int)
+data class PokemonImgSprite(val sprites: Sprites, val name:String, val id: Int, val stats: ArrayList<Stats>, val types: ArrayList<Types>)
 
 data class Sprites(val other:Other)
 data class Other(@SerializedName("official-artwork") val official_artwork: OfficialArtwork, val home: Home)
-data class OfficialArtwork(val front_default: String)
-data class Home(val front_default: String)
+data class OfficialArtwork(val front_default: String, val front_shiny:String)
+data class Home(val front_default: String, val front_shiny: String)
+data class Stats(val base_stat: Int)
+data class Types(val type: Type)
+data class Type(val name: String)
--- app/src/main/java/com/miguel/pokefan/SecondFragment.kt
+++ app/src/main/java/com/miguel/pokefan/SecondFragment.kt
@@ -1,12 +1,16 @@
 package com.miguel.pokefan
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.miguel.pokefan.databinding.FragmentSecondBinding
+import com.miguel.pokefan.utilidades.Busquedas
+import kotlinx.coroutines.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,8 +18,8 @@ import com.miguel.pokefan.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
-    lateinit var id_pokemon: String
-
+    var id_pokemon: String = ""
+    var busqueda = Busquedas()
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -34,9 +38,13 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         if (arguments != null){
             id_pokemon = arguments?.getString("ID_Pokemon").toString()
-            println("ID es: "+id_pokemon)
+            lifecycleScope.launch {
+                busqueda.Pokedex(
+                pokemon = id_pokemon,
+                binding = binding,
+                context = requireContext()
+            ) }
         }
-
     }
 
     override fun onDestroyView() {
